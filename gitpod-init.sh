e5d7ca82d5f8c76806995f191f95108d55c3f3d6
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2022-03-05 21:37:53 +0300

    Add files via upload

---
+++ gitpod-init.sh
@@ -0,0 +1,10 @@
+mysql -u root -e "create database test" &&
+cp .env.example .env &&
+sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env &&
+sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env &&
+sudo apt install php-gmp &&
+composer install &&
+npm i && npm run dev &&
+php artisan migrate &&
+php artisan key:generate 
+
