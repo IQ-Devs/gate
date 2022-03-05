70e5f23e8f8e934c1837d16d05e570c61c7ca411
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2022-03-05 22:40:21 +0300

    Update .gitpod-init.sh

--- .gitpod-init.sh
+++ .gitpod-init.sh
@@ -2,7 +2,6 @@ mysql -u root -e "create database test" &&
 cp .env.example .env &&
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env &&
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env &&
-sudo apt install php-gmp -y &&
 composer install &&
 npm i && npm run dev &&
 php artisan migrate &&
