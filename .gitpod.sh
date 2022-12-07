a0356bfb383ba2863e40e350f2b237a86a096b04
ibrah3m <ibrahem.developer@gmail.com>
2022-12-07 22:37:52 +0000

    upgrade gitpod settings :)

--- .gitpod.sh
+++ .gitpod.sh
@@ -1,2 +1,28 @@
-#first if its both before or init
-#if before only
+
+#if before
+if [ $TASK = "before" ]; then
+if [ $GITPOD_IDE_ALIAS = "phpstorm" ]; then echo -e 'alias npm="ddev exec npm " \n alias php="ddev exec php"  ' >> ~/.bashrc  ;fi
+apt-get update
+apt-get install -y ddev
+ddev start -y
+export DDEV_NONINTERACTIVE=true
+ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env"
+ddev exec 'sed -i "s#APP_URL=.*#APP_URL=${DDEV_PRIMARY_URL}#g" .env'
+ddev exec "php artisan key:generate"
+ddev exec "php artisan migrate"
+ddev exec "php artisan db:seed"
+ddev exec "php artisan storage:link"
+
+    fi
+
+#both
+ddev exec "composer install"
+
+# if init
+if [ $TASK = "init" ]; then
+
+ddev   exec "npm i -g npm@latest"
+ddev   exec "npm install"
+ddev   exec "npm audit fix --force"
+ddev   exec "npm run dev"
+    fi
